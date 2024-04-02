ae11a524a4e7e5c4c8a4ba3aa70aeef8cdd17068
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-04-02 08:43:27 +0100

    Add postgres config schema

--- app/config/db.js
+++ app/config/db.js
@@ -1,4 +1,24 @@
+const Joi = require('joi')
 const { DefaultAzureCredential } = require('@azure/identity')
+
+const schema = Joi.object({
+  postgresConnectionOptions: Joi.object({
+    type: Joi.string().required(),
+    host: Joi.string().required(),
+    port: Joi.number().required(),
+    user: Joi.string().required(),
+    password: Joi.string().required(),
+    database: Joi.string().required()
+  }).required(),
+  tableName: Joi.string().default('knowledge_vectors'),
+  columns: Joi.object({
+    idColumnName: Joi.string().default('id'),
+    vectorColumnName: Joi.string().default('vector'),
+    contentColumnName: Joi.string().default('content'),
+    metadataColumnName: Joi.string().default('metadata')
+  }).required(),
+  aadEndpoint: Joi.string().default('https://ossrdbms-aad.database.windows.net')
+})
 
 const config = {
   postgresConnectionOptions: {
@@ -8,24 +28,23 @@ const config = {
     user: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB
-  },
-  tableName: 'mcu_knowledge_vectors',
-  columns: {
-    idColumnName: 'id',
-    vectorColumnName: 'vector',
-    contentColumnName: 'content',
-    metadataColumnName: 'metadata'
   }
+}
+
+const { error, value } = schema.validate(config)
+
+if (error) {
+  throw new Error(`Postgres config validation error: ${error.message}`)
 }
 
 const getConfig = async () => {
   if (process.env.NODE_ENV === 'production') {
     const credential = new DefaultAzureCredential()
-    const { token } = await credential.getToken('https://ossrdbms-aad.database.windows.net', { requestOptions: { timeout: 1000 } })
-    config.postgresConnectionOptions.password = token
+    const { token } = await credential.getToken(value.aadEndpoint, { requestOptions: { timeout: 1000 } })
+    value.postgresConnectionOptions.password = token
   }
 
-  return config
+  return value
 }
 
 module.exports = {
