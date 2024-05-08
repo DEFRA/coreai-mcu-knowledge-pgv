36c93f09ce7b9847586fcd668c93e7c87577dfa8
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-05-08 12:32:38 +0100

    Add conditional pg password

--- app/config/db.js
+++ app/config/db.js
@@ -7,7 +7,7 @@ const schema = Joi.object({
     host: Joi.string().required(),
     port: Joi.number().required(),
     user: Joi.string().required(),
-    password: Joi.string().required(),
+    password: Joi.string().when('$NODE_ENV', { is: 'production', then: Joi.optional(), otherwise: Joi.required() }),
     database: Joi.string().required()
   }).required(),
   tableName: Joi.string().default('mcu_knowledge_vectors'),
@@ -39,13 +39,13 @@ const config = {
 }
 
 const getConfig = async () => {
+  const { error, value } = schema.validate(config)
+
   if (process.env.NODE_ENV === 'production') {
     const credential = new DefaultAzureCredential()
     const { token } = await credential.getToken(value.aadEndpoint, { requestOptions: { timeout: 1000 } })
-    config.postgresConnectionOptions.password = token
+    value.postgresConnectionOptions.password = token
   }
-
-  const { error, value } = schema.validate(config)
 
   if (error) {
     throw new Error(`Postgres config validation error: ${error.message}`)
@@ -53,6 +53,8 @@ const getConfig = async () => {
 
   return value
 }
+
+getConfig()
 
 module.exports = {
   getConfig
