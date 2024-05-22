d9540ba08cc4f9628676b3ff99191445dd88254d
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-05-22 15:41:20 +0100

    Update MI scope

--- app/config/db.js
+++ app/config/db.js
@@ -43,8 +43,8 @@ const getConfig = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     console.log('Using managed identity for authentication')
-    const credential = new DefaultAzureCredential()
-    const { token } = await credential.getToken('https://ossrdbms-aad.database.windows.net', { requestOptions: { timeout: 1000 } })
+    const credential = new DefaultAzureCredential({ managedIdentityClientId: process.env.AZURE_CLIENT_ID })
+    const { token } = await credential.getToken('https://ossrdbms-aad.database.windows.net/.default', { requestOptions: { timeout: 1000 } })
     config.postgresConnectionOptions.password = token
     console.log('Got PG MI token')
   }
