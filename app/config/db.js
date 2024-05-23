5e8e9b536bc821984d9f665a85fc0528b328d9df
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-05-23 11:18:00 +0100

    Move db schema validation

--- app/config/db.js
+++ app/config/db.js
@@ -7,7 +7,7 @@ const schema = Joi.object({
     host: Joi.string().required(),
     port: Joi.number().required(),
     user: Joi.string().required(),
-    password: Joi.string().when('$NODE_ENV', { is: 'production', then: Joi.allow(null).allow('').optional(), otherwise: Joi.required() }),
+    password: Joi.string().required(),
     database: Joi.string().required()
   }).required(),
   tableName: Joi.string().default('mcu_knowledge_vectors'),
@@ -39,19 +39,21 @@ const config = {
 
 const getConfig = async () => {
   console.log('Getting Postgres config')
-  const { error, value } = schema.validate(config)
 
   if (process.env.NODE_ENV === 'production') {
     console.log('Using managed identity for authentication')
     const credential = new DefaultAzureCredential({ managedIdentityClientId: process.env.AZURE_CLIENT_ID })
     const { token } = await credential.getToken('https://ossrdbms-aad.database.windows.net/.default', { requestOptions: { timeout: 1000 } })
-    value.postgresConnectionOptions.password = token
-    console.log(`Got token: ${token}`)
+    config.postgresConnectionOptions.password = token
   }
+
+  const { error, value } = schema.validate(config)
 
   if (error) {
     throw new Error(`Postgres config validation error: ${error.message}`)
   }
+
+  console.log(value)
 
   return value
 }
