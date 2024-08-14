6140bca9dc88c7514a830b688709a861bc1085df
Shaun Fitzsimons <87677173+sfitz42@users.noreply.github.com>
2024-08-14 07:36:45 +0100

    Increase Pod CPU-Mem Tier (#18)
    * Increase pod cpu-mem tier to XL
    
    * Fix Postgres MI scope
    
    * Use team MI for db connection
    
    * Increase pod cpu-mem tier to XXL
    
    * Switch to workload identity
    
    * Fix workload identity
    
    * Add correct scope
    
    * Use ChainedTokenCredential
    
    * Revert "Use ChainedTokenCredential"
    
    This reverts commit e74b7a10fa429b5cc9b8c48ac92fb9b61b741dba.
    
    * Add demo token aqusition
    
    * Switch to getBearerTokenProvider
    
    * Temp log metadata
    
    * Add scope
    
    * Temp log metadata
    
    * Include  blob metadata

--- app/storage/knowledge-document-repo.js
+++ app/storage/knowledge-document-repo.js
@@ -13,7 +13,7 @@ const listKnowledge = async (search = '', category = '', orderBy = 'lastModified
     includeDeleted: false,
     includeDeletedWithVersions: false,
     includeLegalHold: false,
-    includeMetadata: false,
+    includeMetadata: true,
     includeSnapshots: true,
     includeTags: true,
     includeUncommitedBlobs: false,
@@ -23,6 +23,8 @@ const listKnowledge = async (search = '', category = '', orderBy = 'lastModified
 
   for await (const blob of knowledgeContainer.listBlobsFlat(listOptions)) {
     blob.metadata = mapMetadataToBase(blob.metadata)
+
+    console.log(blob.metadata)
 
     blobs.push(blob)
   }
